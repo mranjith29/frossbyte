1cb141e4147bc2ee252fa4b784c0df772875596f
Ranjith Kumar <103306689+mranjith29@users.noreply.github.com>
2025-10-04 14:52:38 +0530

    Comment out Script import in Main.tsx
    Comment out the import statement for Script.

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,7 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 //import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Script from 'next/script';
+//import Script from 'next/script';
 
 const MAX_DISPLAY = 5
 
